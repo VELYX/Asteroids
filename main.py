1b0a606c6cbc6c1742355a57a1e987bb96b25417
VELYX <vanya.yepez@gmail.com>
2025-09-11 23:04:28 -0600

    Added asteroid field

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 
 def main():
@@ -18,12 +20,16 @@ def main():
     # Group game objects for scalability
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
     # Assign containers, disregard pyright error
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
 
     # Instantiate Player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     # Start game loop, yes it is infinite
     running = True
