f90161123930da99cbaf35380bd88989ecd0abc9
VELYX <vanya.yepez@gmail.com>
2025-09-11 18:59:52 -0600

    Implemented pygame groups to handle escalability of game objects

--- main.py
+++ main.py
@@ -4,23 +4,47 @@ from player import Player
 
 
 def main():
+    # Start Asteroids
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+
+    # Initialize configuration fields
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+
+    # Group game objects for scalability
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    # Assign containers, disregard pyright error
+    Player.containers = (updatable, drawable)
+
+    # Instantiate Player
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    # Start game loop, yes it is infinite
     running = True
     while running:
+        # Set game to 60 fps
         dt = clock.tick(60) / 1000
+
+        # Until we quit the game
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        # Set canvas
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+
+        # Update game objects using their respective groups
+        updatable.update(dt)
+        for obj in drawable:
+            obj.draw(screen)
+
+        # Refresh the screen
         pygame.display.flip()
 
 
